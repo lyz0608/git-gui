5c60d8b4ee54fab8ce9726aec3863dc30fced630
129157326@qq.com <129157326@qq.com>
2024-10-12 15:47:21 +0300

    conflict resolved

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
 int main(){
     char name[20];
+    std::cout<<"hello world! have a nice day!\n";
     cout<<"Enter your name:";
     cin >> name;
-    cout<<"hello,"<<name<<"!\n";
-    cout << "nice to see you learning Git\n";
+    cout <<"hello," <<name<<"!\n";
+    cout <<"nice to see you learning Git\n";
     system("pause");
     return 0;
 }
