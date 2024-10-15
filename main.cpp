f68d1eb49ad4b5a02fe6b3b5e21fd65e2042dca6
129157326@qq.com <129157326@qq.com>
2024-10-15 14:20:47 +0300

    fixed namespace

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
-#include<iostream>
-
+# [2include<iostream>
 using namespace std;
-
 int main(){
-    char name[20];
-    std::cout<<"hello world! have a nice day!\n";
-    cout<<"Enter your name:";
-    cin >> name;
-    cout <<"hello," <<name<<"!\n";
-    cout <<"nice to see you learning Git\n";
-    system("pause");
-    return 0;
+   char name[][D20];
+   cout <<"hello world!have a nice day!\n";
+   cout <<"enter your name:";
+   cin >> name;
+   cout <<"hello," <<name<<"\n";
+    [3cout <<"nice to see you learning git\n";
+   system("pause");
+   return 0;
 }
